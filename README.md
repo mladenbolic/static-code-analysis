421968b7f1b6d570fc1e4b6fe9b8a3a335d05907
Mladen Bolic <mladen.bolic@sixhours.io>
2018-09-19 15:57:58 +0200

    Update static code analysis configuration

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# static-code-analysis
+# Static Code Analysis
+
+Application used and exmaple for setting static code analysis for Java projects.
+> More information can be found on a [blog post](https://medium.com/p/3428dc79f7f9). 
+
+
+## Running Checks
+
+    ./gradlew clean test
+    
+## Building Application
+
+    ./gradlew clean build
--- src/main/java/io/sixhours/analysis/App.java
+++ src/main/java/io/sixhours/analysis/App.java
@@ -1,11 +1,13 @@
 package io.sixhours.analysis;
 
+// NOTE: we do not need to suppress TooManyMethods here, it is here for illustration purposes only
 @SuppressWarnings("PMD.TooManyMethods")
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
+    // Example of suppressing the SystemPrintln
     @SuppressWarnings("PMD.SystemPrintln")
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
