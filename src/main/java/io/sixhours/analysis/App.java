14ad87ed9f8e7a3440877db3f8a1a6b94d0022d4
Mladen Bolic <mladen.bolic@sixhours.io>
2018-09-19 14:17:06 +0200

    Add example of suppressing PMD analysis errors

--- src/main/java/io/sixhours/analysis/App.java
+++ src/main/java/io/sixhours/analysis/App.java
@@ -1,10 +1,12 @@
 package io.sixhours.analysis;
 
+@SuppressWarnings("PMD.TooManyMethods")
 public class App {
-    private String getGreeting() {
+    public String getGreeting() {
         return "Hello world.";
     }
 
+    @SuppressWarnings("PMD.SystemPrintln")
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
